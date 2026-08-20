021116a6eb3119017206cb195224edde1294d127
Dibyajyoti Mishra <mishradibyajyoti011@gmail.com>
2026-08-20 16:22:33 +0530

    Fixed error of reseting

--- Prime Number Identifier/prime_num_identifier__v2.c
+++ Prime Number Identifier/prime_num_identifier__v2.c
@@ -4,20 +4,20 @@ int main(){
     int num, isPrime = 1;
 
     while (1)
-    {  
+    {
+        isPrime = 1;
         printf("Enter a num to check wheather it is prime or not: ");
+
         if(scanf("%d", &num) != 1){
             printf("Invalid input!\n");
-            return 1;
+            while(getchar() != '\n');
+            continue;
         }
         
         if(num == 0 || num == 1){
             isPrime = 0;
-            printf("This is not a prime num\n");
-            return 1;
-        }
-        else{
-            isPrime = 1;
+            printf("This is not a prime num, try greater than 1\n");
+            continue;
         }
         
         for (int i = 2; i * i <= num; i++)
@@ -26,12 +26,9 @@ int main(){
                 isPrime = 0;
                 break;
             }
-            else{
-                isPrime = 1;
-            }
         }
         
-        if(isPrime == 1){
+        if(isPrime){
             printf("This is a prime num\n");
         }
         else{
